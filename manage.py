316395f502e38728994e54e118d2868c9e8dbee3
joltcops <pracheta@kgpian.iitkgp.ac.in>
2024-06-09 12:26:47 +0530

    Correcting mistakes

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 from django.core.management import execute_from_command_line
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LIS.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
