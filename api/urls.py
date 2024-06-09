e6e516e5c9564127a55400b74145e14490ed4cd0
joltcops <pracheta@kgpian.iitkgp.ac.in>
2024-06-09 12:52:57 +0530

    Making no mistakes

--- api/urls.py
+++ api/urls.py
@@ -7,8 +7,6 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns=[
-    path('admin/', admin.site.urls),
-    path('', include('api.urls')),
     path('', views.index, name='index'),
     path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
